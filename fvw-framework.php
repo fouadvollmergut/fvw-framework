5cab2b9f57de2ec1da3cd5343495a9cdf394d794
moritzgvt <hello@moritzgut.de>
2021-10-21 17:02:21 +0200

    Deprecate old update plugin in favor of Github updater class.

--- fvw-framework.php
+++ fvw-framework.php
@@ -18,11 +18,9 @@
   define( 'FVW_FRAMEWORK_RESOURCE_ID', '3' );
 
 
-  /* UPDATER */
+  /* ICLUDE UPDATER */
 
-  require_once FVW_FRAMEWORK_BASE_PATH . 'updater/plugin-update-checker.php';
-
-  $updater = Puc_v4_Factory::buildUpdateChecker( 'https://update.fouadvollmer.de/plugin/fvw-framework/', __FILE__, 'fvw-framework' );
+  require_once FVW_FRAMEWORK_BASE_PATH . 'classes/updater.php';
 
 
   /* INCLUDE CLASSES */
@@ -141,6 +139,25 @@
   } );
 
 
+  /* UPDATER */
+
+  if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
+    $config = array(
+      'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
+      'proper_folder_name' => 'fvw-framework', // this is the name of the folder your plugin lives in
+      'api_url' => 'https://api.github.com/repos/fouadvollmer/fvw-framework', // the GitHub API url of your GitHub repo
+      'raw_url' => 'https://raw.githubusercontent.com/fouadvollmer/fvw-framework/master', // the GitHub raw url of your GitHub repo
+      'github_url' => 'https://github.com/fouadvollmer/fvw-framework/', // the GitHub url of your GitHub repo
+      'zip_url' => 'https://github.com/fouadvollmer/fvw-framework/zipball/master', // the zip url of the GitHub repo
+      'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+      'requires' => '3', // which version of WordPress does your plugin require?
+      'tested' => '5.8.1', // which version of WordPress is your plugin tested up to?
+      'readme' => 'README.md', // which file to use as the readme for the version number
+      'access_token' => '', // Access private repositories by authorizing under Plugins > GitHub Updates when this example plugin is installed
+    );
+
+    new WP_GitHub_Updater($config);
+  }
 
 
 
